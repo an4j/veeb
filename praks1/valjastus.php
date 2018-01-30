8c27c4a3b292ccca0f5fac5e35cddc474d2576da
anneli.jarvi <anneli.jarvi@khk.ee>
2018-01-30 11:23:42 +0200

    ternary kasutamine

--- praks1/valjastus.php
+++ praks1/valjastus.php
@@ -11,11 +11,11 @@
  * kommentaar
  */
 // väljastamine
-echo "Aine - veebiprogrammeerimine"."<br/>"; // käsu lõpp - ;
-print "valikaine<bt/>";
+echo 'Aine - veebiprogrammeerimine'.'<br/>'; // käsu lõpp - ;
+print "valikaine<br/>";
 // muutujate defineerimine
-$eesNimi = "Anneli"; // eesnimi, string
-$pereNimi = "Jarvi"; // perenimi, string
+$eesNimi = 'Anneli'; // eesnimi, string
+$pereNimi = 'Jarvi'; // perenimi, string
 $bussiNumber = 4; // bussi number, integer
 $soiduKestvus = 0.8; // soidu kestvus, float
 // konstantite defineerimine
@@ -23,6 +23,16 @@ define("NUMBER_PI", 3.13);
 /*Kasutades eel defineeritud muutujad ja konstandid
 *valjasta ilus teksti
  */
-echo "Olen ".$eesNimi.' '.$pereNimi.'<br/>';
-echo "Sõidan koolist kesklinna bussiga number".$bussiNumber.'<br/>';
+echo 'Olen '.$eesNimi.' '.$pereNimi.'<br/>';
+echo 'Sõidan koolist kesklinna bussiga number'.$bussiNumber.'<br/>';
+echo 'Jouan kesklinna'.$soiduKestvus.'<br/>';
 echo "Midagi konstantset? PI= ".NUMBER_PI."<br/>";
+//tingimuslaused
+$ilmValjas = 'Sajab lund';
+if ($ilmValjas == 'Sajab lund'){
+    echo 'Tuleb minna sauna<br/>';
+} else {
+    echo 'Pole motet sauna minna<br/>';
+}
+// ? operaator - ternary operator
+echo ($ilmValjas == 'Sajab lund') ? 'Tuleb minna sauna<br/>' : 'Pole motet sauna minna<br/>';

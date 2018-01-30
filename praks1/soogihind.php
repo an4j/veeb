732580ac26e6d45a0937e35b32af61763bf22cf6
anneli.jarvi <anneli.jarvi@khk.ee>
2018-01-30 11:31:10 +0200

    vaartused

---
+++ praks1/soogihind.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anneli.jarvi
+ * Date: 30.01.2018
+ * Time: 11:28
+ */
+// vajalikud muutujad
+$soogiHind = 2.55; // hind, EUR
+$soodustusProtsent = 15; // %
+$toetus = 1.80; // EUR

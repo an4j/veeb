145120409fd52265d55d2546f842a6ca49acc355
anneli.jarvi <anneli.jarvi@khk.ee>
2018-01-30 11:40:19 +0200

    soogihind

--- praks1/soogihind.php
+++ praks1/soogihind.php
@@ -9,3 +9,11 @@
 $soogiHind = 2.55; // hind, EUR
 $soodustusProtsent = 15; // %
 $toetus = 1.80; // EUR
+// arvutused
+if ($soogiHind > 0 and $soodustusProtsent > 0 and $toetus > 0) {
+    $soodustus = $soogiHind * $soodustusProtsent / 100;
+    $hindOpilasele = $soodustus - $toetus.'<br/>';
+    echo 'Soogihind opilasele = '.$hindOpilasele.'<br/>';
+} else {
+    echo 'Andmed ei ole taielikud<br/>';
+}

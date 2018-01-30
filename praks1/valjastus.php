0e62b25cee9a7130815109f1fb2a441a16118e3c
anneli.jarvi <anneli.jarvi@khk.ee>
2018-01-30 10:41:59 +0200

    valjastamine

---
+++ praks1/valjastus.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anneli.jarvi
+ * Date: 30.01.2018
+ * Time: 10:24
+ */
+// üherealine kommentaar
+/* mitme
+* realine
+ * kommentaar
+ */
+// väljastamine
+echo "Aine - veebiprogrammeerimine"; // käsu lõpp - ;
+?>

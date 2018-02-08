3bc1d57634d0219770b1ec85eb3b04077dab35cf
anneli.jarvi <anneli.jarvi@khk.ee>
2018-02-08 11:00:29 +0200

    soogihind

--- praks1/soogihind.php
+++ praks1/soogihind.php
@@ -5,6 +5,34 @@
  * Date: 30.01.2018
  * Time: 11:28
  */
+// funktsioon soodustuse arvutamiseks
+function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
+    // funktsiooni sisu
+    $soodusProtsent = 15; //
+    $soodusHind = $taisHind;
+
+    if ($soodusKaart == true){
+        $soodusHind = $taisHind * ((100 - $soodusProtsent) / 100);
+    }
+    if ($kasOledOpilane){
+        $soodusHind = $soodusHind - $toetus;
+        // soodushind - opilastoetus; // - operaatori lühendatud kuju
+    }
+    return $soodusHind;
+}// funtktsiooni lõpp
+
+// kutsume funktsiooni tööle
+// kui oled opilane
+$soogiHind = soogiHind( taishind 2.65, $soodusKaart true, $kasOledOpilane true);
+echo 'Prae hind õpilasele = '.round($soogiHind, precision 2 ).'€<br/>';
+// kui olemas kliendikaart
+$soogiHind = soogiHind( taishind 2.65, $soodusKaart true, $kasOledOpilane false);
+echo 'Prae hind sooduskaarti omanikule = '.round($soogiHind, precision 2 ).'€<br/>';
+// kui ei ole kliendikaarti
+$soogiHind = soogiHind( taishind 2.65, $soodusKaart false, $kasOledOpilane false);
+echo 'Prae hind = '.round($soogiHind, precision 2 ).'€<br/>';
+
+
 // vajalikud muutujad
 $soogiHind = 2.55; // hind, EUR
 $soodustusProtsent = 15; // %

58359ef2084f2cd44b5c0c4ca27d25391e198cc5
Anneli Järvi <anneli.jarvi@khk.ee>
2018-05-30 20:02:33 +0300

    welcome nimetatud post-iks

--- blog/controllers/welcome.php
+++ blog/controllers/welcome.php
@@ -1,8 +1,6 @@
 <?php namespace App;
-
-class welcome extends Controller
+class posts extends Controller
 {
-
     /**
      * This is a normal action which will be called when user visits /welcome/index URL. Since index is the default
      * action name, it may be omitted (URL can be /welcome). Since welcome is by default the default controller, it may
@@ -11,38 +9,27 @@ class welcome extends Controller
      */
     function index()
     {
-
         $this->users = get_all("SELECT * FROM users");
     }
-
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
      */
     function AJAX_success()
     {
-
-
         stop(201,'Everything is awesome');
     }
-
     /**
      * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
      */
     function AJAX_error()
     {
-
         // Test sending emails
         Mail::send(DEVELOPER_EMAIL, 'test', 'test');
-
         echo "This text comes from the server and will be shown only in development environment for debugging purposes. ";
         echo "Here is a nice exception for you to debug:";
-
         // Generate error for testing
         throw new \Exception('This is a test');
-
-
     }
-
     /**
      * This function will only be ran in case of POST request
      */

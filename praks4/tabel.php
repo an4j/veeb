6e3fe3a9e732a8755ffd76bc461964353768a0a0
Anneli Järvi <anneli.jarvi@khk.ee>
2018-05-30 12:32:12 +0300

    tabeli loomine

---
+++ praks4/tabel.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anneli.jarvi
+ * Date: 30.05.2018
+ * Time: 12:25
+ */
+class tabel {
+    var  $tabeliSisu = array();
+    var $pealkirjad = array();
+    var $veergudearv;
+    public function __construct(array $pealkirjad) {
+        $this->pealkirjad = $pealkirjad;
+        $this->veergudearv = count($pealkirjad);
+    }
+    function lisarida($rida) {
+        if(count($rida) != $this->veergudearv) {
+            return false;
+        }
+        array_push($this->tabeliSisu,$rida);
+        return true;
+        function prindiTabel() {
+            echo '<table border="1">';
+            echo '<tr>';
+            foreach ($this->pealkirjad as $pealkiri) {
+                echo '<th bgcolor="'.$this->taustaVarv.'">'.$pealkiri.'</th>';
+            }
+            echo "\tr";
+            foreach ($this->tabeliSisu as $reaNumber=>$rida) {
+                echo '<tr>';
+                foreach ($rida as $reaElement) {
+                    echo '<td bgcolor="'.$this->taustaVarv.'">'.$reaElement.'</td>';
+                }
+                echo "\tr";
+            }
+            echo '</table>';
+        }

cb32f4fe39c3a6c576f71dc8a0befd1a7a8467ba
Anneli Järvi <anneli.jarvi@khk.ee>
2018-05-30 12:24:08 +0300

    testi loomine

---
+++ praks4/testTabel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anneli.jarvi
+ * Date: 29.05.2018
+ * Time: 12:17
+ */
+require_once 'htmlTable.php';
+$htmlTabel = new htmlTabel(array('a','b','c'), '#ff0000');
+$htmlTabel->lisarida(array(1,2,3));
+$htmlTabel->lisarida(array(4,5,6));
+echo '<pre>';
+print_r($htmlTabel);
+echo '</pre>';
+$htmlTabel->prindiTabel();

2f3fbb5d8052da574e0c72d357e415d9eb19bb3d
Anneli Järvi <anneli.jarvi@khk.ee>
2018-05-30 12:14:24 +0300

    failide yhendamiseks

---
+++ praks3/aeg.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anneli.jarvi
+ * Date: 29.05.2018
+ * Time: 21:47
+ */
+require_once 'funktsioonid.php';
+require 'ab_fnk.php';
+loeVormFailist('aegvorm.html');
+uhendus();
+send();
+selectALL();
